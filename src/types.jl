c04657041836eacdd44073d911c077c7ee578f18
Léo Petit <leo.petit@live.fr>
2022-05-02 14:47:10 +0200

    show methods and getindex

--- src/types.jl
+++ src/types.jl
@@ -8,50 +8,58 @@ Maybe(T::Type) = Union{T,Nothing}
 
 abstract type DatasetType end
 
+function group(ds::DatasetType ; rounding_digits=nothing)
+    if !isnothing(rounding_digits) 
+        @transform!(ds.data, $(ds.group_on) = round($(ds.group_on), digits=rounding_digits))
+    end
+    return isnothing(ds.group_on) ? [ds.data] : groupby(ds.data,ds.group_on)
+end
 
+name(ds::DatasetType) = ds.name
+
+function Base.show(io::IO, ds::DatasetType)
+    print(io, typeof(ds) ," : ", name(ds), ", with target {",ds.target...,"} and grouped on ", ds.group_on, ", ", ds.data)
+end
+function Base.show(io::IO, ::MIME"text/plain", ds::DatasetType)
+    println(io, typeof(ds), " :")
+    println(io, "    -> name       : ", name(ds))
+    println(io, "    -> target     : {", ds.target...,"}")
+    println(io, "    -> grouped on :  ", ds.group_on)
+    println(io, "    -> data       : ", ds.data)
+end
 ### PonctualDataset ###
-struct PonctualDataset <: DatasetType
+struct PonctualDataset <: DatasetType 
+    name::String
     data::DataFrame
     target::Vector{Symbol}
-    function PonctualDataset(data, target ; keys::Vector{tK}=Symbol[]) where tK<:Union{String,Symbol}
-        check_keys(names(data),keys)
-        new(data, target)
-    end
+    group_on::Union{Symbol,Nothing}
 end
-# function PonctualDataset(data, target ; keys::Vector{tK}=Symbol[]) where tK<:Union{String,Symbol}
-#     check_keys(names(data),keys)
-#     PonctualDataset(data, target)
-# end
-PonctualDataset(data, target::Symbol) = PonctualDataset(data, [target])
+PonctualDataset(name, data, target::Symbol, group_on) = PonctualDataset(name, data, [target], group_on)
+PonctualDataset(data, target, group_on) = PonctualDataset("ponctual_$(rand(1:999))", data, target, group_on)
 
 Base.length(pd::PonctualDataset) = size(pd.data,1)
 
 ### TimeseriesDataset ###
 struct TimeseriesDataset <: DatasetType
+    name::String
     data::DataFrame
     target::Vector{Symbol}
-    var::Symbol
-    function TimeseriesDataset(data, target, var ; keys::Vector{tK}=Symbol[]) where tK<:Union{String,Symbol}
-        check_keys(names(data),keys)
-        new(data, target, var)
+    group_on::Union{Symbol,Nothing}
+    function TimeseriesDataset(name,data,target,group_on)
+        @assert ("t" ∈ names(data)) "time column :t is missing from data when constructing TimeseriesDataset"
+        new(name,data,target,group_on)
     end
 end
-
 # function TimeseriesDataset(data, target, var ; keys::Vector{tK}=Symbol[]) where tK<:Union{String,Symbol}
 #     check_keys(names(data),keys)
 #     TimeseriesDataset(data, target, var)
 # end
-TimeseriesDataset(data, target::Symbol, var) = TimeseriesDataset(data, [target], var)
-
-# helper functions :
-function group(ds::TimeseriesDataset ; rounding_digits=1)
-    @transform!(ds.data, $(ds.var) = round($(ds.var), digits=rounding_digits))
-    groupby(ds.data,ds.var)
-end
+TimeseriesDataset(name, data, target::Symbol, group_on) = TimeseriesDataset(name, data, [target], group_on)
+TimeseriesDataset(data, target, group_on) = TimeseriesDataset("timeseries_$(rand(1:999))", data, target, group_on)
 
 function get_timeseries(tsds::TimeseriesDataset,val)
     df = tsds.data
-    return TimeseriesDataset(df[df[:,tsds.var] .== val,:], tsds.target, tsds.var)
+    return TimeseriesDataset(df[df[:,tsds.group_on] .== val,:], tsds.target, tsds.group_on)
 end
 
 Base.length(ts::TimeseriesDataset) = size(ts.data,1)
@@ -61,6 +69,11 @@ Base.length(ts::TimeseriesDataset) = size(ts.data,1)
 Base.@kwdef struct Dataset
     ponctual::Vector{PonctualDataset} = PonctualDataset[]
     timeseries::Vector{TimeseriesDataset} = TimeseriesDataset[]
+    function Dataset(ponctual, timeseries)
+        all_names = [name.(timeseries) ; name.(ponctual)]
+        @assert (length(all_names) == length(unique(all_names))) "datasets names are not unique"
+        new(ponctual, timeseries)
+    end
 end
 Dataset(tsd::Vector{TimeseriesDataset},pd::Vector{PonctualDataset}) = Dataset(pd,tsd)
 Dataset(pd::PonctualDataset,tsd::TimeseriesDataset) = Dataset([pd],[tsd])
@@ -70,17 +83,19 @@ Dataset(tsd::TimeseriesDataset, pd::Vector{PonctualDataset}) = Dataset(pd,[tsd])
 Dataset(pd::PonctualDataset,tsd::Vector{TimeseriesDataset}) = Dataset([pd],tsd)
 Dataset(tsd::Vector{TimeseriesDataset}, pd::PonctualDataset) = Dataset([pd],tsd)
 
+# helpers
 ndatasets(ds::Dataset) = length(ds.ponctual) + length(ds.timeseries)
 Base.length(ds::Dataset) = sum(length(ds.ponctual)) + sum(length(ds.timeseries))
+Base.names(ds::Dataset) = [name.(ds.timeseries) ; name.(ds.ponctual)]
 
-
-function check_keys(colnames::Vector{String},keys::Vector{tK}) where tK<:Union{String,Symbol}
-    if !isempty(keys)
-        isin = colnames .∈ Ref(string.(keys))
-        id_notin = findall(.!isin)
-        !isempty(id_notin) && throw(ArgumentError("column names $(colnames[id_notin]) are in keys argument"))
+function Base.getindex(ds::Dataset, targetname::String)
+    for (i,ts_name) in enumerate(name.(ds.timeseries))
+        (targetname == ts_name) && return ds.timeseries[i]
     end
+    for (i,pd_name) in enumerate(name.(ds.ponctual))
+        (targetname == pd_name) && return ds.ponctual[i]
+    end
+    throw(ArgumentError("unknown dataset name $targetname"))
 end
 
-
-export DatasetType, PonctualDataset, TimeseriesDataset, Dataset, group, get_timeseries, ndatasets
+export DatasetType, PonctualDataset, TimeseriesDataset, Dataset, group, get_timeseries, ndatasets, name
